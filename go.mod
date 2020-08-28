54f1ab0b9ca261148a4c637dc98964908492bcf9
Andy Jones <andy@teller.io>
2020-08-28 23:43:46 +0100

    Got it basically working

--- go.mod
+++ go.mod
@@ -4,14 +4,14 @@ go 1.15
 
 require (
 	github.com/99designs/gqlgen v0.12.2
-	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.3.0 // indirect
 	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
 	github.com/golang-migrate/migrate/v4 v4.12.2
 	github.com/google/uuid v1.1.1
 	github.com/graph-gophers/graphql-go v0.0.0-20200819123640-3b5ddcd884ae // indirect
-	github.com/graphql-go/graphql v0.7.9 // indirect
-	github.com/graphql-go/handler v0.2.3 // indirect
+	github.com/graphql-go/graphql v0.7.9
+	github.com/graphql-go/handler v0.2.3
 	github.com/jinzhu/gorm v1.9.16
 	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/lib/pq v1.3.0
--- main.go
+++ main.go
@@ -1,37 +1,38 @@
 package main
 
 import (
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
 	migrate "github.com/andyjones11/graphql-users/bin"
-	db "github.com/andyjones11/graphql-users/db"
-	"github.com/andyjones11/graphql-users/graph"
-	"github.com/andyjones11/graphql-users/graph/generated"
+	"github.com/andyjones11/graphql-users/db"
 	"github.com/andyjones11/graphql-users/repo"
 	"github.com/gin-gonic/gin"
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/handler"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"log"
 	"os"
 )
 
 const defaultPort = ":8080"
 
-func graphqlHandler(repo *repo.Repositories) gin.HandlerFunc {
-	resolvers := &graph.Resolver{Repo: repo}
-	h := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: resolvers}))
-
+func graphqlHandler(h *handler.Handler) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		h.ServeHTTP(c.Writer, c.Request)
 	}
 }
 
-func playgroundHandler() gin.HandlerFunc {
-	h := playground.Handler("Graphql", "/query")
-
-	return func(c *gin.Context) {
-		h.ServeHTTP(c.Writer, c.Request)
-	}
+type Resolver struct {
+	Repo *repo.Repositories
 }
 
+func (r *Resolver) ListUsersResolver(p graphql.ResolveParams) (interface{}, error) {
+	users, _ := r.Repo.User.ListAllUsers()
+	return users, nil
+}
+
+func (r *Resolver) RegisterUserResolver(p graphql.ResolveParams) (interface{}, error) {
+	user, err := r.Repo.User.CreateUser(p.Args["email"].(string))
+	return user, err
+}
 
 func main() {
 	if len(os.Args) > 1 {
@@ -42,14 +43,61 @@ func main() {
 		}
 	}
 
-	db, _ := db.Connect()
-	defer db.Close()
+	db_conn, _ := db.Connect()
+	db_conn.LogMode(true)
+	defer db_conn.Close()
 
-	repositories, _ := repo.NewRepositories(db)
+	repositries, _ := repo.NewRepositories(db_conn)
+	resolver := Resolver{Repo: repositries}
+
+	userType := graphql.NewObject(graphql.ObjectConfig{
+		Name:        "User",
+		Description: "A User",
+		Fields: graphql.Fields{
+			"id":        &graphql.Field{Type: graphql.NewNonNull(graphql.String)},
+			"email":     &graphql.Field{Type: graphql.NewNonNull(graphql.String)},
+			"createdAt": &graphql.Field{Type: graphql.NewNonNull(graphql.String)},
+			"updatedAt": &graphql.Field{Type: graphql.NewNonNull(graphql.String)},
+		},
+	})
+
+	queryFields := graphql.Fields{
+		"listUsers": &graphql.Field{
+			Type:        graphql.NewList(userType),
+			Description: "List of all users",
+			Resolve:     resolver.ListUsersResolver,
+		},
+	}
+
+	mutationFields := graphql.Fields{
+		"registerUser": &graphql.Field{
+			Type:        userType,
+			Description: "Register a new user",
+			Args: graphql.FieldConfigArgument{
+				"email": &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.String)},
+			},
+			Resolve: resolver.RegisterUserResolver,
+		},
+	}
+
+	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: queryFields}
+	rootMutation := graphql.ObjectConfig{Name: "RootMutation", Fields: mutationFields}
+	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery), Mutation: graphql.NewObject(rootMutation)}
+	schema, err := graphql.NewSchema(schemaConfig)
+
+	if err != nil {
+		log.Fatalf("failed to create new schema. Error: %v", err)
+	}
+
+	h := handler.New(&handler.Config{
+		Schema:     &schema,
+		Pretty:     true,
+		GraphiQL:   true,
+		Playground: false,
+	})
 
 	r := gin.Default()
-	r.POST("/query", graphqlHandler(repositories))
-	r.GET("/", playgroundHandler())
+	r.POST("/graphql", graphqlHandler(h))
+	r.GET("/graphql", graphqlHandler(h))
 	r.Run(defaultPort)
 }
-
